d07fe7e0d1a3dac5ab0ccaf6efe8b520610d3770
luqeey <115131091+luqeey@users.noreply.github.com>
2023-11-23 17:43:27 +0100

    zakladne vecicky porobene co je zapisovanie casu na page

--- index.php
+++ index.php
@@ -1,20 +1,41 @@
+<?php
+session_start();
+
+function addToHistory($entry) {
+    if(!isset($_SESSION['history'])) {
+        $_SESSION['history'] = array();
+    }
+    array_push($_SESSION['history'], $entry);
+}
+
+if(isset($_POST['addEntry'])) {
+    addToHistory(date('Y-m-d, H:i:s'));
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP Akaemia</title>
 </head>
 <body>
-    <form action="functions.php" method="get">
-        <input type="text" name="num01" placeholder="Number 1">
-        <select name="oper">
-            <label>Choose operation!</label>
-            <option value="add">Add</option>
-            <option value="sub">Substract</option>
-        </select>
-        <input type="text" name="num02" placeholder="Number 2">
-        <button type="submit">Calculate</button>
+
+<h1>Prichody</h1>
+    <form method="post">
+        <input type="submit" name="addEntry" value="Add Entry">
+        <ul id="historyList">
+        <?php 
+        echo "Current Date and Time: " . date('Y-m-d, H:i:s');
+            if(isset($_SESSION['history'])) {
+                foreach($_SESSION['history'] as $entry) {
+                    echo "<li>$entry</li>";
+                }
+            }
+        ?>
+        </ul>
     </form>
+
 </body>
 </html>
