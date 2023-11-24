faade9a2990011edb7a016634beb88cf137a0e50
luqeey <115131091+luqeey@users.noreply.github.com>
2023-11-24 22:07:15 +0100

    feedback zapracovany

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
+define('TXT_FILE_PATH', 'session_data.txt');
+
 session_start();
 $h = date("H");
+$delay = false;
 
 date_default_timezone_set( 'CET' );
 
-function isDelay($h) {
-    $delay = false;
-    if($h >= 8) {
+function isDelay($h, &$delay) {
+    if ($h >= 8) {
         $delay = true;
     }
 }
@@ -16,35 +18,35 @@ function addToHistory($entry) {
     if(!isset($_SESSION['history'])) {
         $_SESSION['history'] = array();
     }
-    array_push($_SESSION['history'], $entry);
+    $_SESSION['history'][] = $entry;
 
 }
 
-function saveSessionToTxt($h) {
-    $txtFilePath = 'session_data.txt';
+function saveSessionToTxt($h, &$delay) {
     if($h >= 20 && $h <= 24) {
         die("Nepodarilo sa zapisat cas pretoze je neplatny.");
     }
 
-    if($delay = true) {
-        file_put_contents($txtFilePath, date('d-m-Y, H:i:s') . " - meskanie\n", FILE_APPEND);
+    if($delay == true) {
+        file_put_contents(TXT_FILE_PATH, date('d-m-Y, H:i:s') . " - meskanie\n", FILE_APPEND);
     } else {
-        file_put_contents($txtFilePath, date('d-m-Y, H:i:s') . "\n", FILE_APPEND);
+        file_put_contents(TXT_FILE_PATH, date('d-m-Y, H:i:s') . "\n", FILE_APPEND);
     }
 }
 
 function getLogs() {
     $txtFilePath = 'session_data.txt';
 
-    if(file_exists($txtFilePath)) {
-        $txtContent = file_get_contents($txtFilePath);
+    if(file_exists(TXT_FILE_PATH)) {
+        $txtContent = file_get_contents(TXT_FILE_PATH);
         echo nl2br("$txtContent\n");
     }
 }
 
 if (isset($_POST['addEntry'])) {
     addToHistory(date('d-m-Y, H:i:s'));
-    saveSessionToTxt($h);
+    saveSessionToTxt($h, $delay);
+    isDelay($h, $delay);
 }
 
 
@@ -64,14 +66,12 @@ if (isset($_POST['addEntry'])) {
 <h1>Prichody</h1>
     <form method="post">
         <input type="submit" name="addEntry" value="Add Entry">
-        <ul id="historyList">
         <?php 
         date_default_timezone_set( 'CET' );
         echo "Current Date and Time: " . date('d-m-Y, H:i:s');
         echo "</br>";
         getLogs();
         ?>
-        </ul>
     </form>
 
 </body>
